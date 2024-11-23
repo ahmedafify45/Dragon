6ecc2456998d1d60206571b81b0545daa1490fee
ahmedafify45 <125831172+ahmedafify45@users.noreply.github.com>
2024-11-24 00:08:05 +0200

    Update prodact.service.ts

--- src/servise/prodact.service.ts
+++ src/servise/prodact.service.ts
@@ -8,7 +8,7 @@ export class ProdactService {
   constructor(private http: HttpClient) {}
 
   getAllProducts(): Observable<any[]> {
-    return this.http.get<any[]>('https://run.mocky.io/v3/f9f08b2e-5e2c-494a-bf34-63718139df2f');
+    return this.http.get<any[]>('https://run.mocky.io/v3/7c87926b-4333-450e-ac72-1a7caf5fce0a');
   }
   getProductsById(id: any) {
     return this.http.get('https://run.mocky.io/v3/' + id);
