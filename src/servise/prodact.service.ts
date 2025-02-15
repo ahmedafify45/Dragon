b3717f254a0847e121958e1f8413db684bd952b7
Ahmed Ashraf Elsayed <125831172+ahmedafify45@users.noreply.github.com>
2025-02-15 18:29:42 +0200

    Update prodact.service.ts

--- src/servise/prodact.service.ts
+++ src/servise/prodact.service.ts
@@ -8,7 +8,7 @@ export class ProdactService {
   constructor(private http: HttpClient) {}
 
   getAllProducts(): Observable<any[]> {
-    return this.http.get<any[]>('https://run.mocky.io/v3/7c87926b-4333-450e-ac72-1a7caf5fce0a');
+    return this.http.get<any[]>('https://run.mocky.io/v3/774fdda9-6a23-4502-aefa-443666ddbf0d');
   }
   getProductsById(id: any) {
     return this.http.get('https://run.mocky.io/v3/' + id);
